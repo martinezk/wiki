88285c46f951795cbd270e7e4b46b98ddc7be902
martinezk <misskristinamartinez@gmail.com>
2018-05-11 11:38:41 -0400

    Install simple form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
+
   def index
+  end
+
+  def show
   end
 
   def new
@@ -18,5 +23,9 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
