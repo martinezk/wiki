e38c06488570e1626259e7b53eecba9ac53fc594
martinezk <misskristinamartinez@gmail.com>
2018-05-11 19:49:26 -0400

    Add edit and destroymethods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,55 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show]
-  before_action :authenticate_user!, except: [:index, :show]
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
-  def index
-    if params[:category].blank?
-      @articles = Article.all.order("created_at DESC")
-    else
-      @category_id = Category.find_by(name: params[:category]).id
-      @articles = Article.where(category_id: @category_id).order("created_at DESC")
-    end
-  end
+	def index
+		if params[:category].blank?
+			@articles = Article.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@articles = Article.where(category_id: @category_id).order("created_at DESC")
+		end
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def new
-    @article = current_user.articles.build
-  end
+	def new
+		@article = current_user.articles.build
+	end
 
-  def create
-    @article = current_user.articles.build(article_params)
-    if @article.save
-      redirect_to @article
-    else
-      render 'new'
-    end
-  end
+	def create
+		@article = current_user.articles.build(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
 
-  private
-  def article_params
-    params.require(:article).permit(:title, :content, :category_id)
-  end
+	def edit
+	end
 
-  def find_article
-    @article = Article.find(params[:id])
-  end
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :content, :category_id)
+	end
 end
